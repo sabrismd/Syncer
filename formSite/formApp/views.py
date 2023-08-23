2cd47d999f330ee6c060aedc9de815a30c6ce337
sabrismd <syedsabri3333@gmail.com>
2023-08-23 14:49:52 +0530

    views.py is updated

--- formSite/formApp/views.py
+++ formSite/formApp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 import smartsheet
 import mysql.connector as mc
 
-global token,host,user,pwd,dbname
+global token,host,user,pwd,dbname,connection,client
 # Create your views here.
 def home(request):
     return render(request,'home.html')
@@ -18,6 +18,7 @@ def display_token(request):
         return HttpResponse("Token not provided")
 
 def display_sheet_user(request):
+    global client
     client=smartsheet.Smartsheet(token)
     user_profile=client.Users.get_current_user()
     if 'result' in user_profile.to_dict():
@@ -32,16 +33,40 @@ def server_details(request):
     return render(request,"server_details.html")
 
 def server_connection(request):
-    global host,user,pwd,dbname
+    global host,user,pwd,dbname,connection
     host=request.GET["hostname"]
     user=request.GET["username"]
     pwd=request.GET["password"]
     dbname=request.GET["databasename"]
-    try:
-        connection=mc.connect(host=host,user=user,password=pwd,db=dbname)
-        if connection.is_connected():
-            return HttpResponse("<h1>SUCCESSFULLY CONNECTED TO DATABASE</h1>")
-    except mc.errors.DatabaseError:
-        return HttpResponse("<h1>Can't Connect To a Database, Please Check Your host/userName/pwd/databaseName and Ensure started the mysql server </h1>")
+    connection=mc.connect(host=host,user=user,password=pwd,db=dbname)
+    if connection.is_connected():
+        return render(request,"success_server_connection_status.html")
+    else:
+        return render(request,"failed_server_connection_status.html")
+    
+def tables(request):
+    sheet=client.Sheets.list_sheets(include_all=True)
+    sheets={'sheet':[]}
+    for j in sheet.data:
+        sheets['sheet'].append(j.name)
+    cursor=connection.cursor()
+    cursor.execute("SHOW TABLES")
+    tables = {'table': []}
+    for x in cursor:
+        tables['table'].append(x[0])
+    context={
+        'sheets':sheets,
+        'tables':tables,
+    }
+    return render(request,"database_details.html",context=context)
 
+def selected(request):
+    tab=request.GET["tab"]
+    sh=request.GET["sh"]
+    selected={'table':tab,'sheet':sh}
+    return render(request,"selected.html",context=selected)
+
+
+
+        
 
